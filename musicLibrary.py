abb6cb8be87f761c56660b34430500514b8632be
Mozeel-Hashmi <pysphere1@gmail.com>
2024-12-24 02:31:09 -0800

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,6 +1,6 @@
 music = {
     "meta" : "https://www.youtube.com/watch?v=CNs6WCB5g6s",
-    "universo" : "https://www.youtube.com/watch?v=XmH6pIio0DQ",
+    "funk" : "https://www.youtube.com/watch?v=XmH6pIio0DQ",
     "light" : "https://www.youtube.com/watch?v=r4dnLWt9HXk",
     "moments" : "https://www.youtube.com/watch?v=yJ9C_Ud2enI"
 }
